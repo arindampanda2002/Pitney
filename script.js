fa600d48a5e972c292805d52ef70b29e129340d0
arip995 <arindampanda2002@gmail.com>
2022-03-30 17:54:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 3000); 
+}
+
+
+function scrollPortfolio() {
+  window.scrollBy(0, 1100);
+}
+
+
+function scrollServices() {
+  window.scrollBy(0, 1800);
+}
+
+function scrollBlog() {
+  window.scrollBy(0, 2600);
+}
